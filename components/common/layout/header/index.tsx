b1ced0fa950e8b5b96783371c6eb62ba519b0705
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-06-01 16:48:12 +0900

    design: `Header` 스타일 수정

--- components/common/layout/header/index.tsx
+++ components/common/layout/header/index.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const handleTitleClick = () => window.location.reload();
 
   return (
-    <header className='flex items-center h-16 px-32'>
+    <header className='flex items-center min-h-16 px-32'>
       <Button onClick={handleTitleClick}>
         <h1 className='text-2xl font-black'>스미스</h1>
       </Button>
