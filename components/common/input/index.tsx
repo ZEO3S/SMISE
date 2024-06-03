8f6136a7ab6a9b086f1660f5d8ef44440be75f1b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-06-03 15:59:45 +0900

    refactor: `Input`, `SearchBar` 컴포넌트 구조 변경

--- components/common/input/index.tsx
+++ components/common/input/index.tsx
@@ -1,56 +1,58 @@
 import Image from "next/image";
 import {
-  ChangeEventHandler,
+  ComponentPropsWithoutRef,
   KeyboardEventHandler,
   MouseEventHandler,
-  useState,
 } from "react";
 
 import Button from "../button";
+import Text from "../text";
 
-interface Props {
+interface Props extends ComponentPropsWithoutRef<"input"> {
+  label?: string;
   hasButton?: boolean;
   buttonImage?: any;
-  onEnter?: (value: string) => void;
+  onEnter?: () => void;
+  onClickButton?: () => void;
 }
 
 export default function Input({
+  label = "",
+  value,
   hasButton = false,
   buttonImage = "",
+  onChange,
   onEnter,
+  onClickButton,
+  ...rest
 }: Props) {
-  const [value, setValue] = useState<string>("");
-
-  const handleChangeInput: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setValue(event.target.value);
-  };
-
   const handleKeyDownEnter: KeyboardEventHandler<HTMLInputElement> = (
     event
   ) => {
     if (event.key !== "Enter") return;
 
-    if (onEnter) onEnter(value);
+    if (onEnter) onEnter();
   };
 
   const handleClickButton: MouseEventHandler<HTMLButtonElement> = () => {
-    if (onEnter) onEnter(value);
+    if (onClickButton) onClickButton();
   };
 
   return (
-    <div className='flex flex-1 justify-between px-6 py-3 rounded-full bg-default-color bg-opacity-10'>
+    <label className='flex flex-1 justify-between px-6 py-3 rounded-full bg-default-color bg-opacity-10'>
+      {label && <Text content={label} />}
       <input
         className='flex-1 bg-default-color bg-opacity-0 placeholder:text-default-color placeholder:text-opacity-50 outline-none'
         value={value}
-        placeholder='검색어를 입력해 주세요.'
-        onChange={handleChangeInput}
+        onChange={onChange}
         onKeyDown={handleKeyDownEnter}
+        {...rest}
       />
       {hasButton && (
         <Button onClick={handleClickButton}>
           <Image src={buttonImage} alt='검색 버튼' />
         </Button>
       )}
-    </div>
+    </label>
   );
 }
--- components/searchBar/index.tsx
+++ components/searchBar/index.tsx
@@ -4,15 +4,26 @@ import Input from "../common/input";
 import MagnifyingGlassSVG from "@/assets/svgs/magnifying_glass.svg";
 
 import { Keyword } from "@/types/api/recruitment";
+import { useControlledInput } from "@/hooks/useControlledInput";
+import { useUpdateKeyword } from "@/hooks/useUpdateKeyword";
 
 interface Props {
   setKeyword: Dispatch<SetStateAction<Keyword>>;
 }
 
 export default function SearchBar({ setKeyword }: Props) {
-  const updateKeyword = (value: string) => setKeyword(value);
+  const { value, onChangeInput } = useControlledInput();
+  const { updateKeyword } = useUpdateKeyword(setKeyword);
 
   return (
-    <Input onEnter={updateKeyword} hasButton buttonImage={MagnifyingGlassSVG} />
+    <Input
+      value={value}
+      buttonImage={MagnifyingGlassSVG}
+      placeholder='검색어를 입력해 주세요.'
+      onChange={onChangeInput}
+      onEnter={() => updateKeyword(value)}
+      onClickButton={() => updateKeyword(value)}
+      hasButton
+    />
   );
 }
