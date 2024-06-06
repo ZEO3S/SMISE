5bf1d7b3d9948a76698fd826830255c117e009c2
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-06-06 15:34:58 +0900

    feat: 학력 데이터 추가

--- constants/components/educationLevel.ts
+++ constants/components/educationLevel.ts
@@ -6,7 +6,40 @@ export const EDUCATION_LEVELS: Array<SelectOption> = [
     label: "학력 무관",
   },
   {
-    value: "고교 졸업",
-    label: "고교 졸업",
+    value: "고등학교 중퇴",
+    label: "고등학교 중퇴",
+  },
+  {
+    value: "고등학교 졸업",
+    label: "고등학교 졸업",
+  },
+
+  {
+    value: "대학 중퇴",
+    label: "대학 중퇴",
+  },
+  {
+    value: "대학 휴학",
+    label: "대학 휴학",
+  },
+  {
+    value: "대학 졸업예정",
+    label: "대학 졸업예정",
+  },
+  {
+    value: "대학 졸업(2, 3년)",
+    label: "대학 졸업(2, 3년)",
+  },
+  {
+    value: "대학 졸업(4년)",
+    label: "대학 졸업(4년)",
+  },
+  {
+    value: "석사",
+    label: "석사",
+  },
+  {
+    value: "박사",
+    label: "박사",
   },
 ];
