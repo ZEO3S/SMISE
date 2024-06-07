f8a4a86a782e84d1ba95196270e45209a3d2e6e3
jeujeon <1132jjw@naver.com>
2024-06-07 15:07:06 +0900

    feature: 직무 필터링 구현

--- routers/recruitments.py
+++ routers/recruitments.py
@@ -4,6 +4,7 @@ from models.recruitments import Recruitment
 from datas.military_data import cached_recruitments
 from datas.connection import get_session
 from sqlmodel import select
+from sqlalchemy import and_, or_
 from typing_extensions import Annotated
 
 recruitment_router = APIRouter(
@@ -13,13 +14,13 @@ recruitment_router = APIRouter(
 @recruitment_router.get("/", response_model=List[Recruitment])
 async def retrieve_all_recruitments(
     session=Depends(get_session),
-    service_types: Annotated[Union[List[str], None], Query()] = None,
+    # service_types: Annotated[Union[List[str], None], Query()] = None,
+    service_type: Union[str, None] =  Query(default=None),
     service_status: Union[str, None] = Query(default=None),
-    job: Union[str, None] = Query(default=None),
-    detailed_jobs: Union[str, None] = Query(default=None),
-    locations: Union[str, None] = Query(default=None),
-    experience_level: Union[str, None] = Query(default=None),
+    job: Annotated[Union[List[str], None], Query()] = None,
+    locations: Annotated[Union[List[str], None], Query()] = None,
     education_level: Union[str, None] = Query(default=None),
+    experience_level: Union[str, None] = Query(default=None),
     sort: Union[str, None] = Query(default=None),
     keyword: Union[str, None] = Query(default=None),
     cursor: int = Query(1, description="Page number", ge=1),
@@ -29,14 +30,28 @@ async def retrieve_all_recruitments(
     query = select(Recruitment)
     
     # 필터 조건 추가
-    if service_types:
-        query = query.where(Recruitment.service_type.in_(service_types))
+    if service_type:
+        # query = query.where(Recruitment.service_type.in_(service_types))
+        query = query.where(Recruitment.service_type == service_type)
     if service_status:
         query = query.where(Recruitment.service_status == service_status)
     if job:
-        query = query.where(Recruitment.job == job)
-    if detailed_jobs:
-        query = query.where(Recruitment.detailed_jobs == detailed_jobs)
+        conditions = []
+        for j in job:
+            job_info = j.split(',')
+            category = job_info.pop(0)
+            if job_info[0] == '전체':
+                conditions.append(and_(Recruitment.job == category))
+            else:
+                details = job_info
+                conditions.append(
+                    and_(
+                        Recruitment.job == category,
+                        Recruitment.job_detail.in_(details)
+                    )
+                )
+        query = query.where(or_(*conditions))
+        
     if locations:
         query = query.where(Recruitment.locations == locations)
     if experience_level:
@@ -46,7 +61,6 @@ async def retrieve_all_recruitments(
         
     if keyword:
         query = query.where(
-            # Recruitment.job_skill.contains(keyword) |
             Recruitment.title.contains(keyword) |
             Recruitment.company.contains(keyword)
             )
