518b38ad9ea168f60510526a5cb02a58c36485db
1132jjw <62981031+1132jjw@users.noreply.github.com>
2024-06-09 00:13:22 +0900

    백엔드 브랜치 develop 브랜치에 병합 (#1)
    * Initial commit
    
    * tmp
    
    * feature: connect to db
    
    * feature: 데이터베이스 정합성 검증 코드 작성
    
    * feature: 검색, 정렬 기능 구현
    
    * feature: 직무 리스트 반환 구현
    
    * feature: 직무 필터링 구현
    
    * feature: 지역 필터링 구현
    
    * feature: 경력 필터 구현
    
    * feature: 학력 필터 구현
    
    * feature: 공고 get api 완료
    
    * feature: 공고 get api 코드 스타일 변경
    
    * style: 폴더명 변경
    
    * chore: 중복 파일 삭제
    
    * style: 폴더 이동
    
    * style: 폴더 이동
    
    * build: dockerfile, environment.yml 생성
    
    * chore: git ignore 추가
    
    * Initial commit
    
    ---------
    
    Co-authored-by: 전증훈 <jeonjeunghoon@gmail.com>

---
+++ backend/app/models/recruitments.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class Recruitment(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    service_type: str
+    service_status: str
+    job: str
+    job_detail: str
+    experience_level: str
+    education_level: str
+    expiration_date: str
+    updated_date: str
+    title: str
+    company: str
+    location: str
+    salary: str
+    href: str
+    
+    class Config:
+        arbitrary_types_allowd = True
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "serviceType": "산업기능요원",
+                "serviceStatus": "현역",
+                "job": "정보처리업종",
+                "jobDetail": "정보처리",
+                "experienceLevel": "경력무관",
+                "updatedDate": "04/06/2024",
+                "educationLevel": "대학졸업",
+                "expirationDate": "채용시 마감",
+                "title": "머신러닝 엔지니어 채용",
+                "company": "샌드버드",
+                "location": "서울특별시 강남구",
+                "salary": "6000만원",
+                "href": "https://sendbird.com/careers"
+            }
+        }
